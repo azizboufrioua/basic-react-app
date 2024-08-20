7c4f1f5a32d9076f3f60ce22a6814eedc15248da
Aziz <aziz.boufrioua@gmail.com>
2024-08-20 08:32:06 +0100

    first component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import MyFirstComponent from './MyFirstComponent.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <MyFirstComponent></MyFirstComponent>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -21,5 +23,7 @@ function App() {
     </div>
   );
 }
+
+
 
 export default App;
---
+++ src/MyFirstComponent.js
@@ -0,0 +1,12 @@
+function MyFirstComponent()
+{
+  return(<div>
+    <h1>Hello word</h1>
+    <p>
+      I'm a pargraphe in component
+    </p>
+  </div>
+  );
+}
+
+export default MyFirstComponent;
